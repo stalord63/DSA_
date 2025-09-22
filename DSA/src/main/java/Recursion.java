04ef73c96393295f7ae670529ae2efdf8b7cbb25
aditya <adi06031998@gmail.com>
2025-09-23 01:08:00 +0530

    recursion basics

--- DSA/src/main/java/Recursion.java
+++ DSA/src/main/java/Recursion.java
@@ -1,26 +1,63 @@
  class Recursion {
 
-    void print(int i, int n) {                         //PRINT TILL N NAME
+     void print(int i, int n) {                         //PRINT TILL N NAME
          if (i > n)
              return;
          System.out.println("ADITYA");
          print(i + 1, n);
      }
 
+     //*************************************************************************************************
+     void print_till_n(int i, int n) {                    //PRINT FROM 1 TO N
+         if (i > n) {
+             return;
+
+         }
+         System.out.println(i);
+         print_till_n(i + 1, n);
+
+     }
+
+     //*************************************************************************************************
+     void print_nto1(int n, int i) {                    //PRINT FROM N TO 1
+         if (n < 1) {
+             return;
+         }
+         System.out.println(n);
+         print_nto1(n - 1, i);
+     }
+
+     //*************************************************************************************************
+     void print_nto1(int n) {                                  //PRINT FROM N TO 1 WITHOUT I
+         if (n <1) {
+             return;
+         }
+         System.out.println(n);
+         print_nto1(n - 1);
+         n = n - 1;
 
 
+     }
+
+     //*************************************************************************************************
+     void print_1ton(int n) {                                  //PRINT FROM N TO 1 WITHOUT I
+         if (n <1) {
+             return;
+         }
+         print_1ton(n - 1);
+         System.out.println(n);
+
+         n = n - 1;
+
+
+     }
 
 
  }
 
-
-
-
-
-
  public void main(String[] args){
 
     Recursion obj= new Recursion();
-    obj.print(1,7);
+    obj.print_nto1(10);
 
 }
